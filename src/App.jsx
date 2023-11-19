7c2d65416f2e4f83543f75e82bd01c0d0838985d
dev-shubhamsoni <dev.shubhamsoni@gmail.com>
2023-11-19 23:34:43 +0530

    started using vite

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
+
+function App() {
+
+ return (
+   <>
+   <Input />
+   <h1>aledjnjd</h1>
+     <Button>Secondary</Button> <br /><br />
+     <Button variant="destructive">Here</Button> <br /><br />
+     <Button variant="secondary">Secondary</Button> <br /><br />
+     <Button variant="ghost">Secondary</Button> <br /><br />
+   </>
+ )
+}
+
+export default App
